b81a9d6a4ddd07365dab7b7699686150c61026d9
Leonardus Kevin <leonardus.kevin@ui.ac.id>
2020-10-31 19:55:39 +0700

    ggg

--- app/src/main/java/com/example/mealresq/HomeFragment.kt
+++ app/src/main/java/com/example/mealresq/HomeFragment.kt
@@ -4,46 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class HomeFragment: Fragment() {
 
     private lateinit var rootView: View
 
-    private lateinit var rvRestoran: RecyclerView
-    private lateinit var rvRestoranNearMe: RecyclerView
-    private var list: ArrayList<Restaurant> = arrayListOf()
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
         rootView = inflater.inflate(R.layout.home_fragment, container, false)
         setHasOptionsMenu(true)
 
-        rvRestoran = rootView.findViewById(R.id.rv_restoran)
-        rvRestoranNearMe = rootView.findViewById(R.id.rv_restoran2)
-        rvRestoran.setHasFixedSize(true)
-        rvRestoranNearMe.setHasFixedSize(true)
-
-        list.addAll(RestoranData.listData)
-        showRecyclerList()
-
         return rootView
-    }
-
-    private fun showRecyclerList() {
-        rvRestoran.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        rvRestoranNearMe.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        val listRestoranAdapter = ListRestaurantAdapter(list)
-        rvRestoran.adapter = listRestoranAdapter
-        rvRestoranNearMe.adapter = listRestoranAdapter
-
-        listRestoranAdapter.setOnItemClickCallBack(object: ListRestaurantAdapter.OnItemClickCallback {
-            override fun onItemClicked(data: Restaurant) {
-                Toast.makeText(context, data.name, Toast.LENGTH_SHORT).show()
-            }
-        })
     }
 
     companion object{
