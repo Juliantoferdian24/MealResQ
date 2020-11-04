0348463f444ce2c218beaf60021e15302ff21a13
Leonardus Kevin <leonardus.kevin@ui.ac.id>
2020-11-04 15:20:01 +0700

    favorite

--- app/src/main/java/com/example/mealresq/FavFragment.kt
+++ app/src/main/java/com/example/mealresq/FavFragment.kt
@@ -1,20 +1,47 @@
 package com.example.mealresq
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class FavFragment: Fragment() {
 
     private lateinit var rootView: View
+    private lateinit var rvFav: RecyclerView
+    private var list: ArrayList<Restaurant> = arrayListOf()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
         rootView = inflater.inflate(R.layout.fav_fragment, container, false)
         setHasOptionsMenu(true)
 
+        rvFav = rootView.findViewById(R.id.rv_fav)
+        rvFav.setHasFixedSize(true)
+
+        list.addAll(RestoranData.listData)
+        showRecyclerList()
+
         return rootView
+    }
+
+    private fun showRecyclerList() {
+        rvFav.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        val listRestoranAdapter = ListRestaurantAdapter(list)
+        rvFav.adapter = listRestoranAdapter
+
+
+        listRestoranAdapter.setOnItemClickCallBack(object: ListRestaurantAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Restaurant) {
+                val intent = Intent(activity, RestoranActivity::class.java)
+                startActivity(intent)
+                Toast.makeText(context, data.name, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     companion object{
--- app/src/main/java/com/example/mealresq/ProfileFragment.kt
+++ app/src/main/java/com/example/mealresq/ProfileFragment.kt
@@ -1,26 +1,43 @@
 package com.example.mealresq
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.profile_fragment.*
 
-class ProfileFragment: Fragment() {
 
+class ProfileFragment: Fragment(){
     private lateinit var rootView: View
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
+    private lateinit var mgoToCommunity: ImageButton
+    private lateinit var mgoToAboutUs: ImageButton
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         rootView = inflater.inflate(R.layout.profile_fragment, container, false)
         setHasOptionsMenu(true)
+        mgoToCommunity = rootView.findViewById(R.id.goToCommunity)
+        mgoToAboutUs = rootView.findViewById(R.id.goToAboutUs)
 
+        mgoToCommunity.setOnClickListener {
+            val intent = Intent(context, CommunityActivity::class.java)
+            startActivity(intent)
+        }
+
+        mgoToAboutUs.setOnClickListener {
+            val intent = Intent(context, AboutActivity::class.java)
+            startActivity(intent)
+        }
         return rootView
     }
 
-    companion object{
-//        var TAG = ProfileFragment::class.java.simpleName
+
+    companion object {
+        //        var TAG = ProfileFragment::class.java.simpleName
         private const val ARG_POSITION: String = "position"
-        fun newInstance(): ProfileFragment{
+        fun newInstance(): ProfileFragment {
             val fragment = ProfileFragment()
             val args = Bundle()
             args.putInt(ARG_POSITION, 1)
