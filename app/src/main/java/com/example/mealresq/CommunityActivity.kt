91c05c8fb6a47de3217945d21778ef480431001a
Ferdian Julianto <Juliantoferdian24@gmail.com>
2020-11-05 18:51:55 +0700

    Profile screen diperindah, backup favourite sebelum dirombak

--- app/src/main/java/com/example/mealresq/CommunityActivity.kt
+++ app/src/main/java/com/example/mealresq/CommunityActivity.kt
@@ -1,12 +1,35 @@
 package com.example.mealresq
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class CommunityActivity: AppCompatActivity() {
+
+    private lateinit var facebook: ImageView
+    private lateinit var instagram: ImageView
+    private lateinit var twitter: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.community_fragment)
+        facebook = findViewById(R.id.facebook)
+        instagram = findViewById(R.id.instagram)
+        twitter = findViewById(R.id.twitter)
+
+        facebook.setOnClickListener{
+            Toast.makeText(this, "facebook", Toast.LENGTH_SHORT).show()
+        }
+        instagram.setOnClickListener{
+            Toast.makeText(this, "instagram", Toast.LENGTH_SHORT).show()
+        }
+        twitter.setOnClickListener{
+            Toast.makeText(this, "twitter", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
 }
 
--- app/src/main/java/com/example/mealresq/ProfileFragment.kt
+++ app/src/main/java/com/example/mealresq/ProfileFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.profile_fragment.*
@@ -15,21 +16,34 @@ class ProfileFragment: Fragment(){
     private lateinit var rootView: View
     private lateinit var mgoToCommunity: ImageButton
     private lateinit var mgoToAboutUs: ImageButton
+    private lateinit var tgoToCommunity: TextView
+    private lateinit var tgoToAboutUs: TextView
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         rootView = inflater.inflate(R.layout.profile_fragment, container, false)
         setHasOptionsMenu(true)
         mgoToCommunity = rootView.findViewById(R.id.goToCommunity)
         mgoToAboutUs = rootView.findViewById(R.id.goToAboutUs)
 
+        tgoToCommunity = rootView.findViewById(R.id.joinOurCommunity)
+        tgoToAboutUs = rootView.findViewById(R.id.aboutUs)
         mgoToCommunity.setOnClickListener {
             val intent = Intent(context, CommunityActivity::class.java)
             startActivity(intent)
         }
-
         mgoToAboutUs.setOnClickListener {
             val intent = Intent(context, AboutActivity::class.java)
             startActivity(intent)
         }
+
+        tgoToCommunity.setOnClickListener {
+            val intent = Intent(context, CommunityActivity::class.java)
+            startActivity(intent)
+        }
+        tgoToAboutUs.setOnClickListener {
+            val intent = Intent(context, AboutActivity::class.java)
+            startActivity(intent)
+        }
+
         return rootView
     }
 
